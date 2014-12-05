bf117ae126787e8966711bc2b1bf2370a313a432
shineforever <shine_forever@yeah.net>
2014-12-05 16:46:12 +0800

    第8天学习

---
+++ ex33.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+#coding:utf-8
+
+
+i = 0
+numbers = []
+
+while i < 6:
+    print "At the top i is %d" % i
+    numbers.append(i)
+
+
+    i = i + 1
+    print "Numbers now: ", numbers
+
+    print "At the bottom i is %d" % i
+
+
+print "The numberes: "
+
+
+for num in numbers:
+    print num
