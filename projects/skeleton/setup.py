611c916a8277318e38308957b78e1b40c03ec6e2
shineforever <shine_forever@yeah.net>
2014-12-10 16:39:57 +0800

    1210 practise

---
+++ projects/skeleton/setup.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+#coding:utf-8
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+        'description':'My Project',
+        'author':'shine_forever',
+        'url':'URL to get it at.',
+        'download_url':'Where to download it.',
+        'author_email':'xxxx@163.com',
+        'version':'0.1'
+        'install_requires':['nose'],
+        'packages':['NAME'],
+        'scripts':[],
+        'name':'projectname'
+        }
+
+
+setup(**config)
