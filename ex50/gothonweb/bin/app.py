668b4fc4d412f985b286cafb3d14575bb1cd34b2
shineforever <shine_forever@yeah.net>
2014-12-11 14:47:41 +0800

    ex50

---
+++ ex50/gothonweb/bin/app.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+#coding:utf-8
+
+import web
+
+
+urls = (
+        '/','Index'
+        )
+
+
+app = web.application(urls,globals())
+
+render = web.template.render('templates/')
+
+
+
+class Index(object):
+    def GET(self):
+        greeting = "Hello World"
+
+        return render.index(greeting = greeting)
+
+
+if __name__ == '__main__':
+    app.run()
+
