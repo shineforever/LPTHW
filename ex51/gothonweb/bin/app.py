5cc991b4eb426bf90aab2f483b0a311de86a8d1f
shineforever <shine_forever@yeah.net>
2014-12-11 16:53:32 +0800

    ex51

--- ex51/gothonweb/bin/app.py
+++ ex51/gothonweb/bin/app.py
@@ -11,15 +11,22 @@ urls = (
 
 app = web.application(urls,globals())
 
-render = web.template.render('templates/')
+render = web.template.render('templates/',base="layout")
 
 
 
 class Index(object):
     def GET(self):
-        greeting = "Hello, World"
+
+        return render.hello_form()
+
+    def POST(self):
+        form = web.input(name="Nobody",greet="Hello")
+
+        greeting = "%s,%s" % (form.greet,form.name)
 
         return render.index(greeting = greeting)
+
 
 
 if __name__ == '__main__':
